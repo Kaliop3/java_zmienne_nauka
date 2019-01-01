f140572c889751127bc5dd3961f3f985a514dd6c
kaliop3 <klaudiaklason@gmail.com>
2019-01-01 12:15:48 +0100

    add code loops

--- src/pl/klaudialason/programy_JAVA/Main.java
+++ src/pl/klaudialason/programy_JAVA/Main.java
@@ -8,6 +8,14 @@ public class Main {//class
         [] -
         */
         System.out.printf("Hello  Klaudia ");
-    }
 
+     for (int i = 1; i <= 1000; i++) {
+        if ((i % 3 == 0) || (i % 7 == 0)) {
+            System.out.print(i);
+            if (i < 999) {
+                System.out.print(", ");
+            }
+        }
+    }
+    }
 }

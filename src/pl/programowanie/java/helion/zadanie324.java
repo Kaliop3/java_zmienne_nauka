62df43fdcf9e1328a0fb23ca1ae2fddf015d5fe3
kaliop3 <klaudiaklason@gmail.com>
2019-02-07 18:14:11 +0100

    add code in java

--- src/pl/programowanie/java/helion/zadanie324.java
+++ src/pl/programowanie/java/helion/zadanie324.java
@@ -3,16 +3,28 @@ package pl.programowanie.java.helion;
 public class zadanie324 {
     public static void main(String[] args) {
         System.out.println("Program wypisuje algebet zwyczajnie i wspak");
-        char letter = 'Z';
+        char letter;
 
-        do{
+        letter = 'Z';
+        while (letter >= 'A') {
             if (letter > 'A') {
                 System.out.print(letter + ", ");
             } else {
-                System.out.println(letter + ".");
+                System.out.print(letter + ".");
             }
             letter--;
-        }while(letter>='A');
+        }
+        System.out.println();
 
-    }
-}
+        letter = 'A';
+        while (letter <= 'Z') {
+            if (letter < 'Z') {
+                System.out.print(letter + ", ");
+            } else {
+                System.out.print(letter + ".");
+            }
+            letter++;
+        }
+    }}
+
+

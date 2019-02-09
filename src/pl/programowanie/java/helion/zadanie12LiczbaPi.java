aa75d0613a90f6cc61f651658f9e14da837af5c9
kaliop3 <klaudiaklason@gmail.com>
2019-02-09 12:03:09 +0100

    add code in java

--- src/pl/programowanie/java/helion/zadanie12LiczbaPi.java
+++
@@ -1,9 +0,0 @@
-package pl.programowanie.java.helion;
-
-public class zadanie12LiczbaPi {
-    public static void main(String[] args) {
-        System.out.println("Program wyswietli piewiastek z liczby  pi z zadana dokladnoscia");
-        System.out.printf("pierwiastek(Pi) = "+"%3.2f\n", Math.sqrt(Math.PI));
-
-    }
-}
